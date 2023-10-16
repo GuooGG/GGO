cfc47f7c59de5b51f2e5fa7570d925d68ebe89c5
GGo <2311619185@qq.com>
2023-10-16 23:31:24 +0800

    日志模块和其他头唔见成功编译

---
+++ build/CMakeFiles/GGoTest.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/root/workspace/GGoSeverFrame/GGo/GGo.cpp" "CMakeFiles/GGoTest.dir/GGo/GGo.cpp.o" "gcc" "CMakeFiles/GGoTest.dir/GGo/GGo.cpp.o.d"
+  "/root/workspace/GGoSeverFrame/GGo/src/LogSystem.cpp" "CMakeFiles/GGoTest.dir/GGo/src/LogSystem.cpp.o" "gcc" "CMakeFiles/GGoTest.dir/GGo/src/LogSystem.cpp.o.d"
+  "/root/workspace/GGoSeverFrame/GGo/src/config.cpp" "CMakeFiles/GGoTest.dir/GGo/src/config.cpp.o" "gcc" "CMakeFiles/GGoTest.dir/GGo/src/config.cpp.o.d"
+  "/root/workspace/GGoSeverFrame/GGo/src/mutex.cpp" "CMakeFiles/GGoTest.dir/GGo/src/mutex.cpp.o" "gcc" "CMakeFiles/GGoTest.dir/GGo/src/mutex.cpp.o.d"
+  "/root/workspace/GGoSeverFrame/GGo/src/thread.cpp" "CMakeFiles/GGoTest.dir/GGo/src/thread.cpp.o" "gcc" "CMakeFiles/GGoTest.dir/GGo/src/thread.cpp.o.d"
+  "/root/workspace/GGoSeverFrame/GGo/src/util.cpp" "CMakeFiles/GGoTest.dir/GGo/src/util.cpp.o" "gcc" "CMakeFiles/GGoTest.dir/GGo/src/util.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
