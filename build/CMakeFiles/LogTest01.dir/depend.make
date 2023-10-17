1e23686e31a71fbaec8c4710ebc76e3fbe58f22d
GGo <2311619185@qq.com>
2023-10-17 22:50:13 +0800

    删除build log目录

--- build/CMakeFiles/LogTest01.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for LogTest01.
-# This may be replaced when dependencies are built.
--- build/CMakeFiles/LogTest02.dir/GGo/src/mutex.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/LogTest02.dir/GGo/src/mutex.cpp.o: \
- /root/workspace/GGoSeverFrame/GGo/src/mutex.cpp \
- /usr/include/stdc-predef.h \
- /root/workspace/GGoSeverFrame/GGo/include/mutex.h
