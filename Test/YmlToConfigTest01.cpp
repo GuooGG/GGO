8543254c8bb76fd34a8221a460303d8f6c9ced05
GGo <2311619185@qq.com>
2023-10-20 01:05:51 +0800

    编译测试从yml文件中读取基本类型配置系统的功能

--- Test/YmlToConfigTest01.cpp
+++ Test/YmlToConfigTest01.cpp
@@ -24,20 +24,24 @@ void listAllMembers(const std::string &prefix,
 }
 int main()
 {
+    GGo::ConfigVar<int>::ptr g_int_value_config = GGo::Config::Lookup("system.port", (int)8080, "system port");
     YAML::Node node = YAML::LoadFile("/root/workspace/GGoSeverFrame/conf/log.yml");
-    std::cout << "show root:" << std::endl;
-    std::cout << node << std::endl;
+    // std::cout << "show root:" << std::endl;
+    // std::cout << node << std::endl;
     std::list<std::pair<std::string, const YAML::Node>> all_nodes;
     std::cout << "============================" << std::endl;
     listAllMembers("",node,all_nodes);
-    for(auto& pair : all_nodes){
-        std::cout << "path:[" << pair.first << "]  :  val: ["  << pair.second << " ] "<<std::endl;
-    }
-    std::cout << "============================" << std::endl;
+    // for(auto& pair : all_nodes){
+    //     std::cout << "path:[" << pair.first << "]  :  val: ["  << pair.second << " ] "<<std::endl;
+    // }
+    // std::cout << "============================" << std::endl;
+    GGO_LOG_INFO(GGO_LOG_ROOT()) << g_int_value_config->getValue();
     GGo::Config::loadFromYaml(node);
+    GGO_LOG_INFO(GGO_LOG_ROOT()) << g_int_value_config->getValue();
+    std::cout << "============================" << std::endl;
     auto map = GGo::Config::GetDatas();
     for(auto& kv : map){
-        std::cout << kv.first << kv.second << std::endl;
+        std::cout << kv.first << std::endl;
     }
     return 0;
 }
