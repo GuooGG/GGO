27f9d1cd565e8282247ba0f7661b479aa6910101
GGo <2311619185@qq.com>
2023-10-24 23:40:38 +0800

    再次测试线程库的正确性以及一千线程下的计数实验

--- Test/ThreadTest01.cpp
+++ Test/ThreadTest01.cpp
@@ -9,32 +9,46 @@ using std::cout;
 using std::endl;
 
 GGo::Logger::ptr g_logger = GGO_LOG_ROOT();
-static uint64_t count = 0;
 
-void func(){
-    GGO_LOG_INFO(g_logger) << endl << "  name: " << GGo::Thread::GetThisName() << endl
-                           << "  this.name: " << GGo::Thread::GetThis()->getName() <<endl
-                           << "  id: " << GGo::GetThreadID() << endl 
+void func()
+{
+    GGO_LOG_INFO(g_logger) << endl
+                           << "  name: " << GGo::Thread::GetThisName() << endl
+                           << "  this.name: " << GGo::Thread::GetThis()->getName() << endl
+                           << "  id: " << GGo::GetThreadID() << endl
                            << "  this.id: " << GGo::Thread::GetThis()->getID() << endl;
 }
+void test_thread_info(){
+    std::vector<GGo::Thread::ptr> threads;
 
-void countter(){
-    for(int i = 0; i < 1000; i++){
+    for (int i = 0; i < 100; i++)
+    {
+        GGo::Thread::ptr thread(new GGo::Thread(&func, "name_" + std::to_string(i)));
+        threads.push_back(thread);
+    }
+
+    for (size_t i = 0; i < threads.size(); ++i)
+    {
+        threads[i]->join();
+    }
+}
+
+static uint64_t count = 0;
+static GGo::Mutex mutex;
+void countter()
+{
+    for (int i = 0; i < 50; i++)
+    {
         count++;
         GGO_LOG_INFO(g_logger) << count;
     }
 }
-
-
-int main(){
-    GGO_LOG_INFO(g_logger) << "ThreadTest01 Start:";
-    YAML::Node node = YAML::LoadFile("/root/workspace/GGoSeverFrame/conf/log.yml");
-    GGo::Config::loadFromYaml(node);
-
+void test_countter_without_mutex(){
     std::vector<GGo::Thread::ptr> threads;
 
-    for(int i = 0 ;i < 100; i++){
-        GGo::Thread::ptr thread(new GGo::Thread(&countter,"name_" + std::to_string(i)));
+    for (int i = 0; i < 100; i++)
+    {
+        GGo::Thread::ptr thread(new GGo::Thread(&countter, "name_" + std::to_string(i)));
         threads.push_back(thread);
     }
 
@@ -43,6 +57,42 @@ int main(){
         threads[i]->join();
     }
     GGO_LOG_INFO(g_logger) << count;
+}
+void countter_mutex()
+{
+    for (int i = 0; i < 100; i++)
+    {
+        GGo::Mutex::Lock lock(mutex);
+        count++;
+        GGO_LOG_INFO(g_logger) << count;
+    }
+}
+void test_countter_with_mutex()
+{
+    std::vector<GGo::Thread::ptr> threads;
+
+    for (int i = 0; i < 1000; i++)
+    {
+        GGo::Thread::ptr thread(new GGo::Thread(&countter_mutex, "name_" + std::to_string(i)));
+        threads.push_back(thread);
+    }
+
+    for (size_t i = 0; i < threads.size(); ++i)
+    {
+        threads[i]->join();
+    }
+    GGO_LOG_INFO(g_logger) << count;
+}
+
+
+
+int main(){
+    GGO_LOG_INFO(g_logger) << "ThreadTest01 Start:";
+    YAML::Node node = YAML::LoadFile("/root/workspace/GGoSeverFrame/conf/log.yml");
+    GGo::Config::loadFromYaml(node);
+
+    test_countter_with_mutex();
+
     GGO_LOG_INFO(g_logger) << "ThreadTest01 end";
     return 0;
 }
