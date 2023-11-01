4eb1bbecab0c6d2583a8aa9f146dbb7653771e4d
GGo <2311619185@qq.com>
2023-11-01 21:34:12 +0800

    修改测试代码

--- Test/SchedulerTest01.cpp
+++ Test/SchedulerTest01.cpp
@@ -11,17 +11,21 @@ static uint64_t count = 0;
 void printcount(){
     count++;
     GGO_LOG_INFO(g_logger) << "count now= " << count;
+    GGo::Fiber::yieldToReady();
+    count++;
+    GGO_LOG_INFO(g_logger) << "count now= " << count;
 }
-void test_scheduler(){
-    GGo::Scheduler::ptr scheduler(new GGo::Scheduler(10, false, "test scheduler"));
+void test_scheduler(bool use_caller){
+    GGo::Scheduler::ptr scheduler(new GGo::Scheduler(2, use_caller, "test scheduler"));
     GGO_LOG_INFO(g_logger) << "scheduler structed";
     GGO_LOG_INFO(g_logger) << "scheduler start";
     scheduler->start();
     GGO_LOG_INFO(g_logger) << "scheduler started";
-    for(int i = 0;i<1000;i++){
+    for(int i = 0;i<10;i++){
+        GGO_LOG_INFO(g_logger) << "mission " << i << " appended";
         scheduler->schedule(&printcount);
     }
-    GGO_LOG_INFO(g_logger) << "scheduler stopp";
+    GGO_LOG_INFO(g_logger) << "scheduler stop";
     scheduler->stop();
     GGO_LOG_INFO(g_logger) << "scheduler stopped";
 }
@@ -30,6 +34,6 @@ int main(){
     YAML::Node node = YAML::LoadFile("/root/workspace/GGoSeverFrame/Test/conf/log.yml");
     GGo::Config::loadFromYaml(node);
 
-    test_scheduler();
+    test_scheduler(false);
     return 0;
 }
