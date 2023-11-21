844e7acb7650ff880c52503e46255a5391255ac5
GGo <2311619185@qq.com>
2023-11-21 20:44:35 +0800

    hook系统调用

--- GGo/src/hook.cpp
+++ GGo/src/hook.cpp
@@ -25,6 +25,24 @@ void hook_init()
     sleep_f = (sleep_fun)dlsym(RTLD_NEXT, "sleep");
     usleep_f = (usleep_fun)dlsym(RTLD_NEXT, "usleep");
     nanosleep_f = (nanosleep_fun)dlsym(RTLD_NEXT, "nanosleep");
+    socket_f = (socket_fun)dlsym(RTLD_NEXT, "socket");
+    connect_f = (connect_fun)dlsym(RTLD_NEXT, "connext");
+    accept_f = (accept_fun)dlsym(RTLD_NEXT, "accept");
+    read_f = (read_fun)dlsym(RTLD_NEXT, "read");
+    readv_f = (readv_fun)dlsym(RTLD_NEXT, "readv");
+    recv_f = (recv_fun)dlsym(RTLD_NEXT, "recv");
+    recvfrom_f = (recvfrom_fun)dlsym(RTLD_NEXT, "recvfrom");
+    recvmsg_f = (recvmsg_fun)dlsym(RTLD_NEXT, "recvmsg");
+    write_f = (write_fun)dlsym(RTLD_NEXT, "write");
+    writev_f = (writev_fun)dlsym(RTLD_NEXT, "writev");
+    send_f = (send_fun)dlsym(RTLD_NEXT, "send");
+    sendto_f = (sendto_fun)dlsym(RTLD_NEXT, "sendto");
+    sendmsg_f = (sendmsg_fun)dlsym(RTLD_NEXT, "sendmsg");
+    close_f = (close_fun)dlsym(RTLD_NEXT, "close");
+    fcntl_f = (fcntl_fun)dlsym(RTLD_NEXT, "fcntl");
+    ioctl_f = (ioctl_fun)dlsym(RTLD_NEXT, "ioctl");
+    setsockopt_f = (setsockopt_fun)dlsym(RTLD_NEXT, "getsockopt");
+    getsockopt_f = (getsockopt_fun)dlsym(RTLD_NEXT, "getsockopt");
 }
 
 struct __HookIniter
@@ -142,6 +160,24 @@ extern "C"{
 sleep_fun sleep_f = nullptr;
 usleep_fun usleep_f = nullptr;
 nanosleep_fun nanosleep_f = nullptr;
+socket_fun socket_f = nullptr;
+connect_fun connect_f = nullptr;
+accept_fun accept_f = nullptr;
+read_fun read_f = nullptr;
+readv_fun readv_f = nullptr;
+recv_fun recv_f = nullptr;
+recvfrom_fun recvfrom_f = nullptr;
+recvmsg_fun recvmsg_f = nullptr;
+write_fun write_f = nullptr;
+writev_fun writev_f = nullptr;
+send_fun send_f = nullptr;
+sendto_fun sendto_f = nullptr;
+sendmsg_fun sendmsg_f = nullptr;
+close_fun close_f = nullptr;
+fcntl_fun fcntl_f = nullptr;
+ioctl_fun ioctl_f = nullptr;
+setsockopt_fun setsockopt_f = nullptr;
+getsockopt_fun getsockopt_f = nullptr;
 
 unsigned int sleep(unsigned int seconds){
     if(!GGo::t_hook_enable){
