9a438456d962530776475cb48c91d0b4e76bfd92
GGo <2311619185@qq.com>
2023-12-12 21:48:01 +0800

    完成ragel与HTTP解析器整合并测试通过

--- GGo/src/http/httpParser.cpp
+++ GGo/src/http/httpParser.cpp
@@ -108,6 +108,23 @@ HTTPRequestParser::HTTPRequestParser()
     m_parser.data = this;
 }
 
+size_t HTTPRequestParser::excute(char *data, size_t len)
+{
+    size_t offset = http_parser_execute(&m_parser, data, len, 0);
+    memmove(data, data + offset, len - offset);
+    return offset;
+}
+
+int HTTPRequestParser::isFinished()
+{
+    return http_parser_is_finished(&m_parser);
+}
+
+int HTTPRequestParser::hasError()
+{
+    return m_error || http_parser_has_error(&m_parser);
+}
+
 uint64_t HTTPRequestParser::getContentLength()
 {
     return m_request->getHeaderAs<uint64_t>("content-length", 0);
@@ -122,6 +139,12 @@ uint64_t HTTPRequestParser::getHTTPRequestMaxBodySize()
 {
     return s_http_request_max_body_size;
 }
+
+uint64_t HTTPResponseParser::getContentLength()
+{
+    return m_response->getHeaderAs<uint64_t>("content_length", 0);
+}
+
 uint64_t HTTPResponseParser::getHTTPResponseBufferSize()
 {
     return s_http_response_buffer_size;
@@ -129,6 +152,94 @@ uint64_t HTTPResponseParser::getHTTPResponseBufferSize()
 uint64_t HTTPResponseParser::getHTTPResponseMaxBodySize()
 {
     return s_http_request_max_body_size;
+}
+
+void on_response_reason(void* data, const char* at, size_t len){
+    HTTPResponseParser* parser = static_cast<HTTPResponseParser*>(data);
+    parser->getData()->setReasons(std::string(at,len));
+}
+
+void on_response_status(void* data, const char* at, size_t len){
+    HTTPResponseParser* parser = static_cast<HTTPResponseParser*>(data);
+    parser->getData()->setStatus((HTTPStatus)(atoi(at)));
+}
+
+void on_response_chunksize(void* data, const char* at, size_t len){
+    
+}
+
+void on_response_version(void* data, const char* at, size_t len){
+    HTTPResponseParser* parser = static_cast<HTTPResponseParser*>(data);
+    uint8_t version = 0x00;
+    if(strncmp(at, "HTTP/1.1", len) == 0){
+        version = 0x11;
+    }else if(strncmp(at, "HTTP/1.0", len) == 0){
+        version = 0x10;
+    }else{
+        GGO_LOG_WARN(g_logger) << "invalid http response version: "
+                            << std::string(at, len);
+        parser->setError(1001);
+        return;
+    }
+    parser->getData()->setVersion(version);
+}
+
+void on_response_header_done(void *data, const char *at, size_t length) {
+
+}
+
+void on_response_last_chunk(void *data, const char *at, size_t length) {
+
+}
+
+void on_response_http_field(void *data, const char *field, size_t flen
+                           ,const char *value, size_t vlen) {
+    HTTPResponseParser* parser = static_cast<HTTPResponseParser*>(data);
+    if(flen == 0) {
+        GGO_LOG_WARN(g_logger) << "invalid http response field length == 0";
+        parser->setError(1002);
+        return;
+    }
+    parser->getData()->setHeader(std::string(field, flen)
+                                ,std::string(value, vlen));
+}
+
+HTTPResponseParser::HTTPResponseParser()
+    :m_error(0)
+{
+    m_response.reset(new GGo::HTTP::HTTPResponse);
+    httpclient_parser_init(&m_parser);
+
+    m_parser.reason_phrase = on_response_reason;
+    m_parser.status_code = on_response_status;
+    m_parser.chunk_size = on_response_chunksize;
+    m_parser.http_version = on_response_version;
+    m_parser.header_done = on_response_header_done;
+    m_parser.last_chunk = on_response_last_chunk;
+    m_parser.http_field = on_response_http_field;
+    m_parser.data = this;
+
+}
+
+size_t HTTPResponseParser::excute(char *data, size_t len, bool chunck)
+{
+    if(chunck){
+        httpclient_parser_init(&m_parser);
+    }
+    size_t offset = httpclient_parser_execute(&m_parser, data, len, 0);
+
+    memmove(data, data + offset, len - offset);
+    return offset;
+}
+
+int HTTPResponseParser::isFinished()
+{
+    return httpclient_parser_is_finished(&m_parser);
+}
+
+int HTTPResponseParser::hasError()
+{
+    return m_error || httpclient_parser_has_error(&m_parser);
 }
 
 namespace{
--- Test/HTTPTest01.cpp
+++ Test/HTTPTest01.cpp
@@ -2,6 +2,26 @@
 
 #define LOG GGO_LOG_DEBUG(GGO_LOG_ROOT())
 
+const char test_request_data[] = "POST / HTTP/1.1\r\n"
+                                "Host: www.sylar.top\r\n"
+                                "Content-Length: 10\r\n\r\n"
+                                "1234567890";
+const char test_response_data[] = "HTTP/1.1 200 OK\r\n"
+        "Date: Tue, 04 Jun 2019 15:43:56 GMT\r\n"
+        "Server: Apache\r\n"
+        "Last-Modified: Tue, 12 Jan 2010 13:48:00 GMT\r\n"
+        "ETag: \"51-47cf7e6ee8400\"\r\n"
+        "Accept-Ranges: bytes\r\n"
+        "Content-Length: 81\r\n"
+        "Cache-Control: max-age=86400\r\n"
+        "Expires: Wed, 05 Jun 2019 15:43:56 GMT\r\n"
+        "Connection: Close\r\n"
+        "Content-Type: text/html\r\n\r\n"
+        "<html>\r\n"
+        "<meta http-equiv=\"refresh\" content=\"0;url=http://www.baidu.com/\">\r\n"
+        "</html>\r\n";
+
+
 void test_method_to_string(){
     LOG << "test method to string start";
     GGo::HTTP::HTTPMethod method = GGo::HTTP::HTTPMethod::DELETE;
@@ -39,11 +59,45 @@ void test_response(){
     rsp->dump(std::cout) << std::endl;
 }
 
+void test_request_parser(){
+    GGo::HTTP::HTTPRequestParser parser;
+    std::string tmp = test_request_data;
+    size_t s = parser.excute(&tmp[0], tmp.size());
+    LOG << "execute rt=" << s
+        << "has_error=" << parser.hasError()
+        << " is_finished=" << parser.isFinished()
+        << " total=" << tmp.size()
+        << " content_length=" << parser.getContentLength();
+    tmp.resize(tmp.size() - s);
+    LOG << parser.getData()->toString();
+    LOG << "============================";
+    LOG << tmp;
+}
+
+
+void test_response_parser(){
+    GGo::HTTP::HTTPResponseParser parser;
+    std::string tmp = test_response_data;
+    size_t s = parser.excute(&tmp[0], tmp.size(), true);
+    LOG << "execute rt=" << s
+        << " has_error=" << parser.hasError()
+        << " is_finished=" << parser.isFinished()
+        << " total=" << tmp.size()
+        << " content_length=" << parser.getContentLength()
+        << " tmp[s]=" << tmp[s];
+    tmp.resize(tmp.size() - s);
+    LOG << parser.getData()->toString();
+    LOG << "============================";
+    LOG << tmp;
+}
+
 int main(){
     // test_method_to_string();
     // test_string_to_method();
     // test_status_to_string();
     // test_request();
-    test_response();
+    // test_response();
+    // test_request_parser();
+    test_response_parser();
     return 0;
 }
