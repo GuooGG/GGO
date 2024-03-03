3222f732590a529b49ac2ff489084fc2614fe3a5
GGo <2311619185@qq.com>
2024-03-03 18:35:35 +0800

    编写HTTP客户端封装

--- GGo/include/http/httpConnection.h
+++ GGo/include/http/httpConnection.h
@@ -80,13 +80,96 @@ public:
     /// @param timeout_ms 超时时间
     /// @param headers HTTP请求头部参数
     /// @param body 请求消息体
-    /// @return HTTP结果消息体
+    /// @return HTTP结果结构体
     static HTTPResult::ptr sendGet(const std::string& url, uint64_t timeout_ms 
                                     , const std::map<std::string, std::string>& headers = {}
                                     , const std::string& body = "");
     
+    /// @brief 发送HTTP GET请求
+    /// @param uri URL对象
+    /// @param timeout_ms 超时时间
+    /// @param headers HTTP请求头部参数
+    /// @param body 请求消息体
+    /// @return HTTP结果结构体
+    static HTTPResult::ptr sendGet(Uri::ptr url
+                                , uint64_t timeout_ms
+                                , const std::map<std::string, std::string>& headers = {}
+                                , const std::string& body = "");
 
+    /// @brief 发送HTTP POST请求
+    /// @param url 请求的url字符串
+    /// @param timeout_ms 超时时间
+    /// @param headers HTTP请求头部参数
+    /// @param body 请求消息体
+    /// @return HTTP结果结构体
+    static HTTPResult::ptr sendPost(const std::string& url
+                                , uint64_t timeout_ms
+                                , const std::map<std::string, std::string>& headers = {}
+                                , const std::string& body = "");
 
+    /// @brief 发送HTTP POST请求
+    /// @param url URL对象
+    /// @param timeout_ms 超时时间
+    /// @param headers HTTP请求头部参数
+    /// @param body 请求消息体
+    /// @return HTTP结果结构体
+    static HTTPResult::ptr sendPost(Uri::ptr url
+                                , uint64_t timeout_ms
+                                , const std::map<std::string, std::string>& headers = {}
+                                , const std::string& body = "");   
+
+    /// @brief 发送HTTP请求
+    /// @param method 请求类型
+    /// @param uri 请求的URI字符串
+    /// @param timeout_ms 超时时间
+    /// @param headers HTTP请求头部参数
+    /// @param body 请求消息体
+    /// @return HTTP结果结构体
+    static HTTPResult::ptr sendRequest(HTTPMethod method
+                                , const std::string& url            
+                                , uint64_t timeout_ms
+                                , const std::map<std::string, std::string>& headers = {}
+                                , const std::string& body = "");
+    
+    /// @brief 发送HTTP请求
+    /// @param method 请求类型
+    /// @param uri URI对象
+    /// @param timeout_ms 超时时间
+    /// @param headers 请求头部参数
+    /// @param body 请求消息体
+    /// @return HTTP结果结构体
+    static HTTPResult::ptr sendRequest(HTTPMethod method
+                                , Uri::ptr url
+                                , uint64_t timeout_ms
+                                , const std::map<std::string, std::string>& headers = {}
+                                , const std::string& body = "");
+    /// @brief 发送HTTP请求
+    /// @param request 请求结构体
+    /// @param uri URI对象
+    /// @param timeout 超时时间
+    /// @return HTTP结果结构体
+    static HTTPResult::ptr sendRequest(HTTPRequest::ptr request
+                                , Uri::ptr url
+                                , uint64_t timeout);  
+
+    /// @brief 构造函数
+    /// @param sock socket对象
+    /// @param owner 是否对socket拥有所有权
+    HTTPConnection(Socket::ptr sock, bool owner = true);
+
+    /// @brief 析构函数
+    ~HTTPConnection();
+
+    /// @brief 接收HTTP响应
+    HTTPResponse::ptr recvResponse();
+
+    /// @brief 发送HTTP请求
+    /// @param request HTTP请求结构体
+    int sendRequest(HTTPRequest::ptr request);
+
+private:
+    uint64_t m_createTime = 0;
+    uint64_t m_request = 0;
 };
 
 class HTTPConnectionPool{
--- GGo/src/http/httpConnection.cpp
+++ GGo/src/http/httpConnection.cpp
@@ -1,4 +1,5 @@
 #include "http/httpConnection.h"
+#include "httpConnection.h"
 
 
 namespace GGo{ 
@@ -15,7 +16,75 @@ std::string HTTPResult::toString() const
     return ss.str();
 }
 
+HTTPResult::ptr HTTPConnection::sendRequest(HTTPMethod method, const std::string &url, uint64_t timeout_ms, const std::map<std::string, std::string> &headers, const std::string &body)
+{
+    Uri::ptr uri = Uri::Create(url);
+    if(!uri){
+        return std::make_shared<HTTPResult>((int)HTTPResult::Error::INVALID_URL
+                                            , nullptr
+                                            , "invalid url: " + url);
+    }
+    return sendRequest(method, uri, timeout_ms, headers, body);
+}
 
+HTTPResult::ptr HTTPConnection::sendRequest(HTTPMethod method, Uri::ptr url, uint64_t timeout_ms, const std::map<std::string, std::string> &headers, const std::string &body)
+{
+    HTTPRequest::ptr request = std::make_shared<HTTPRequest>();
+    request->setParam(url->getPath());
+    request->setQuery(url->getQuery());
+    request->setFrgment(url->getFragment());
+    request->setMethod(method);
+    bool has_host = false;
+    for(auto& i : headers){
+        if(strcasecmp(i.first.c_str(), "connection") == 0){
+            if(strcasecmp(i.second.c_str(), "keep-alive") == 0){
+                request->setAutoClose(false);
+            }
+            continue;
+        }
+        if(has_host == false && strcasecmp(i.first.c_str(), "host") == 0){
+            has_host = !i.second.empty();
+        }
+        request->setHeader(i.first,i.second);
+    }
+    request->setBody(body);
+    return sendRequest(request, url, timeout_ms);
+}
+
+HTTPResult::ptr HTTPConnection::sendRequest(HTTPRequest::ptr request, Uri::ptr url, uint64_t timeout)
+{
+    bool is_ssl = url->getScheme() == "https";
+    Address::ptr addr = url->createAddress();
+    if(!addr){
+        return std::make_shared<HTTPResult>((int)HTTPResult::Error::INVALID_HOST
+                                            , nullptr
+                                            , "invalid host: " + url->getHost());
+    }
+    Socket::ptr sock = Socket::CreateTCP(addr);
+    if(!sock){
+        return std::make_shared<HTTPResult>((int)HTTPResult::Error::CREATE_SOCKET_ERROR
+                                            , nullptr
+                                            , "create socket fail: " + addr->toString()
+                                            + "errno=" + std::to_string(errno
+                                            + "errstr=" + std::string(strerror(errno))));
+    }
+    //TODO::this
+}
+
+HTTPConnection::HTTPConnection(Socket::ptr sock, bool owner)
+    : SocketStream(sock, owner)
+{
+}
+
+HTTPConnection::~HTTPConnection()
+{
+
+}
+int HTTPConnection::sendRequest(HTTPRequest::ptr request)
+{
+    std::string data = request->toString();
+    return writeFixSize(data.c_str(), data.size());
+}
 }
 }
 
